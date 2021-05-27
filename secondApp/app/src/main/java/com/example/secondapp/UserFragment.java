18e7fed0bcbc94475e36a927f199d14800eae9b3
User <tat3377@yandex.ru>
2021-05-27 14:58:34 +0300

    Изменения

--- secondApp/app/src/main/java/com/example/secondapp/UserFragment.java
+++ secondApp/app/src/main/java/com/example/secondapp/UserFragment.java
@@ -11,24 +11,18 @@ import androidx.fragment.app.Fragment;
 public class UserFragment extends Fragment {
     User user;
     TextView userInfo;
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        user = new User();
-        user.setUserName("USER1");
-        user.setUserLastName("USER_LASTNAME");
-        user.setPhone("79653214657");
+        //int position = getActivity().getIntent().getIntExtra("position",1);
+        int position = getArguments().getInt("position");
+        user = Users.get(getActivity()).getUserList().get(position);
     }
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_user, viewGroup, false);
         userInfo = view.findViewById(R.id.userInfo);
-        userInfo.setText(user.getUserName() + " " + user.getUserLastName() + "\n" + user.getPhone());
+        userInfo.setText(user.getUserName()+" "+user.getUserLastName()+"\n"+user.getPhone());
         return view;
-
-
     }
-
 }
